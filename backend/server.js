1eaa342eb4af6f70fedb693974f716015efc1916
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-02-07 17:35:54 +0530

    feat: Built the backend server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Backend is running on PORT ${PORT}`);
+});
